07a2ddacac5d3817b8e524a70bad7d0a505ebda8
Bohdan <135275568+brightly-shining@users.noreply.github.com>
2023-09-07 00:09:23 +0200

    ⚙️ Configure vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "./",
+  build: {
+    target: "esnext",
+  },
   plugins: [react()],
 });
