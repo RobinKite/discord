35e2380c6e39c119e3e8d86d3a7fddf33b5d848b
Bohdan <135275568+brightly-shining@users.noreply.github.com>
2023-09-07 00:12:04 +0200

    ⚙️ Configure eslint

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { node: true, browser: true, es2020: true },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
@@ -12,6 +12,8 @@ module.exports = {
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
   rules: {
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
