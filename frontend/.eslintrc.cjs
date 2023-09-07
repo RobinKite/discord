5601709e2a17decac1178112b406944dd35827ac
Bohdan <135275568+brightly-shining@users.noreply.github.com>
2023-09-07 03:00:29 +0200

    ⚙️ Configure linters

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   env: { node: true, browser: true, es2021: true },
   extends: [
-    "eslint:recommended",
+    "airbnb",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
@@ -12,6 +12,10 @@ module.exports = {
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
   rules: {
+    "react/jsx-one-expression-per-line": "off",
+    "import/no-unresolved": "off",
+    "import/no-absolute-path": "off",
+    "import/no-extraneous-dependencies": "off",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
