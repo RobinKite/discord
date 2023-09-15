7a43088f5a176f8498f0b4aecbc19d3535e5f84e
Alex Kite <33868185+RobinKite@users.noreply.github.com>
2023-09-15 20:22:51 +0200

    Routing (#10)
    * add favicon and title
    
    * set up design tokens
    
    * router set up v1
    
    * add redirect to route channels
    
    * wip: useRefreshToken

---
+++ src/AppRoutes.jsx
@@ -0,0 +1,48 @@
+import { Route, Routes } from "react-router-dom";
+import PublicLayout from "./components/Layout/PublicLayout";
+import RequireAuth from "./components/RequireAuth/RequireAuth";
+import Default from "./pages/Default/Default";
+import Login from "./pages/Login/Login";
+import Home from "./pages/Home/Home";
+
+const AppRoutes = () => {
+	return (
+		<Routes>
+			<Route
+				exact
+				path="/"
+				element={<PublicLayout />}>
+				<Route
+					path="login"
+					element={<Login />}
+				/>
+
+				<Route element={<RequireAuth />}>
+					<Route
+						path="/"
+						element={<Home />}
+					/>
+					<Route
+						path="channels/@me"
+						element={<p>Channel</p>}
+					/>
+					<Route
+						path="channels/:channelId/:textChannelId"
+						element={<></>}
+					/>
+					<Route
+						path="channels/:channelId/:voiceChannelId"
+						element={<></>}
+					/>
+				</Route>
+
+				<Route
+					path="*"
+					element={<Default />}
+				/>
+			</Route>
+		</Routes>
+	);
+};
+
+export default AppRoutes;
