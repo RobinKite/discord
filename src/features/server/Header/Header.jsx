fce0b832713f0f2cd6e250a84cbb3ad803347a26
Bohdan <135275568+brightly-shining@users.noreply.github.com>
2023-09-18 16:43:45 +0200

    💅 Add fonts

--- src/features/server/Header/Header.jsx
+++ src/features/server/Header/Header.jsx
@@ -3,23 +3,23 @@ import { BiHash, BiSolidHelpCircle } from "react-icons/bi";
 import styles from "./Header.module.css";
 
 export function Header({ serverName, channelName }) {
-  return <header className={styles.header}>
-    <div className={styles.sidebar}>
-      <h2 className={styles.title}>{serverName}</h2>
-    </div>
-    <div className={styles.chat}>
-      <div className={styles.heading}>
-        <BiHash className={styles.hashtag} size={24} />
-        <h2 className={styles.title}>{channelName}</h2>
-      </div>
-      <div className={styles.actions}>
-        <BiSolidHelpCircle className={styles.icon} size={24} />
-      </div>
-    </div>
-  </header>;
+	return <header className={styles.header}>
+		<div className={styles.sidebar}>
+			<h2 className={styles.title}>{serverName}</h2>
+		</div>
+		<div className={styles.chat}>
+			<div className={styles.heading}>
+				<BiHash className={styles.hashtag} size={24} />
+				<h2 className={styles.title}>{channelName}</h2>
+			</div>
+			<div className={styles.actions}>
+				<BiSolidHelpCircle className={styles.icon} size={24} />
+			</div>
+		</div>
+	</header>;
 }
 
 Header.propTypes = {
-  serverName: PropTypes.string,
-  channelName: PropTypes.string
+	serverName: PropTypes.string,
+	channelName: PropTypes.string
 };
