00013562070b3892d0c8dd9610828f2763c85980
Bohdan <135275568+brightly-shining@users.noreply.github.com>
2023-09-19 17:14:45 +0200

    💅 Add fonts & lint files (#14)
    * 💅 Add fonts
    
    * ⚙️ Reconfigure linters

--- src/features/server/Layout/Layout.jsx
+++ src/features/server/Layout/Layout.jsx
@@ -3,23 +3,23 @@ import styles from "./Layout.module.css";
 import ClientSidebar from "@/components/ClientSidebar/ClientSidebar";
 
 export function Layout() {
-	return (
-		<div className="flex">
-			<ClientSidebar />
+  return (
+    <div className="flex">
+      <ClientSidebar />
 
-			<div className={styles.layout}>
-				<Header
-					serverName="Server"
-					channelName="general"
-				/>
-				<main className={styles.content}>
-					<Sidebar
-						fullname="User"
-						username="username"
-					/>
-					<Chat />
-				</main>
-			</div>
-		</div>
-	);
+      <div className={styles.layout}>
+        <Header
+          serverName="Server"
+          channelName="general"
+        />
+        <main className={styles.content}>
+          <Sidebar
+            fullname="User"
+            username="username"
+          />
+          <Chat />
+        </main>
+      </div>
+    </div>
+  );
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,32 +2,32 @@ import useAuth from "@/hooks/useAuth";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const Login = () => {
-	const { setAuth } = useAuth();
+  const { setAuth } = useAuth();
 
-	const navigate = useNavigate();
-	const location = useLocation();
-	const from = location.state?.from?.pathname || "/";
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
 
-	const handleSubmit = () => {
-		try {
-			setAuth({ user: "Alex" }); //replace with real username
-			navigate(from, { replace: true });
-		} catch (e) {
-			console.error(e);
-		}
-	};
+  const handleSubmit = () => {
+    try {
+      setAuth({ user: "Alex" }); //replace with real username
+      navigate(from, { replace: true });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
-	return (
-		<>
-			{/* Replace this with login page */}
-			<p className="text-white text-center mt-7 text-xl">Login Page</p>
-			<button
-				onClick={handleSubmit}
-				className="text-white mx-auto mt-7 text-xl block hover:text-[red]">
+  return (
+    <>
+      {/* Replace this with login page */}
+      <p className="text-white text-center mt-7 text-xl">Login Page</p>
+      <button
+        onClick={handleSubmit}
+        className="text-white mx-auto mt-7 text-xl block hover:text-[red]">
 				Log in
-			</button>
-		</>
-	);
+      </button>
+    </>
+  );
 };
 
 export default Login;
