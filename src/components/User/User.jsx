06fd2537dcd8f54299e17aea5b549a6da92888db
Alex Bird <horoshiy.ab@gmail.com>
2023-09-26 18:05:43 +0300

    update date of birth logic

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,25 +1,45 @@
 import { SiDiscord } from "react-icons/si";
 import { BsCircleFill } from "react-icons/bs";
 import PropTypes from "prop-types";
+import { IoMdMoon } from "react-icons/io";
+import { BsRecordCircleFill } from "react-icons/bs";
+
+const statusMap = {
+  online: <BsCircleFill className="text-[green]" />,
+  offline: <BsCircleFill className="text-stone-500" />,
+  invisible: <BsRecordCircleFill className="text-stone-500" />,
+  idle: <IoMdMoon className="text-yellow-500 rotate-[270deg]" />,
+};
+const offlineRoles = ["offline", "invisible"];
 // import { useSelector } from "react-redux";
 
 export default function User({ user }) {
   // const username = useSelector(state => state?.user?.userName) || "No name";
-  const username = user?.userName || "No name";
-  const userAvatar = user?.userAvatar;
+  const { userName = "No name", userAvatar = null, status, role } = user;
+  const isOffline = !offlineRoles.includes(status);
+
   return (
-    <div className="relative flex cursor-pointer items-center rounded bg-[#2b2d31] px-1.5 py-1 hover:bg-[#35373d]">
-      <div className="mr-2 flex h-[32px] w-[32px] items-center justify-center rounded-[50%] bg-[#5d64f4] ">
+    <div className="flex px-1.5 py-1 rounded items-center bg-[#2b2d31] cursor-pointer hover:bg-[#35373d] relative ">
+      <div className="flex rounded-[50%] bg-[#5d64f4] w-[32px] h-[32px] mr-2 justify-center items-center ">
         {userAvatar ? (
-          <img src={userAvatar} alt="user avatar" />
+          <img
+            src={userAvatar}
+            alt="user avatar"
+          />
         ) : (
-          <SiDiscord size={20} className="text-white" />
+          <SiDiscord
+            size={20}
+            className="text-white"
+          />
         )}
       </div>
-      <span className="absolute bottom-1 left-7 flex h-[14px] w-[14px] items-center justify-center rounded-[50%] bg-inherit p-[3px]">
-        <BsCircleFill className="text-[green]" />
-      </span>
-      <p className="text-[#9b59b6]">{username}</p>
+      {isOffline && (
+        <span className="absolute flex justify-center items-center p-[3px] w-[14px] h-[14px] rounded-[50%] bg-inherit bottom-1 left-7">
+          {statusMap[status]}
+        </span>
+      )}
+
+      <p className="text-[#9b59b6]">{userName}</p>
     </div>
   );
 }
