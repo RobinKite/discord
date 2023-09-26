0eaecdc725d8aa2caf591532c5c167960b46ae97
Alex Bird <horoshiy.ab@gmail.com>
2023-09-26 19:53:39 +0300

    code refactor

---
+++ src/constants/mock.js
@@ -0,0 +1,40 @@
+import { Status } from ".";
+
+export const users = [
+  {
+    userName: "Eric",
+    userId: "1",
+    role: "role-1",
+    status: Status.ONLINE,
+  },
+  {
+    userName: "Stan",
+    userId: "2",
+    role: "role-1",
+    status: Status.OFFLINE,
+  },
+  {
+    userName: "Wendy",
+    userId: "3",
+    role: "role-2",
+    status: Status.IDLE,
+  },
+  {
+    userName: "Kenny",
+    userId: "4",
+    role: null,
+    status: Status.INVISIBLE,
+  },
+  {
+    userName: "Kyle",
+    userId: "5",
+    role: null,
+    status: Status.OFFLINE,
+  },
+  {
+    userName: "Chef",
+    userId: "6",
+    role: null,
+    status: Status.ONLINE,
+  },
+];
