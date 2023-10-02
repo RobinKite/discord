5544d443c81ff8d3f7525c4db0b0a8b8998dee6b
Anastasia Aleksandrova <anastasiayurievnaaleksandrova@gmail.com>
2023-10-03 00:39:35 +0300

    WIP

--- src/forms/RegistrationForm/RegistrationForm.jsx
+++ src/forms/RegistrationForm/RegistrationForm.jsx
@@ -2,7 +2,6 @@ import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { Formik, Form } from "formik";
 import validationSchema from "./validationSchema";
 import { Button, Input } from "@/components";
-import { Button, Input } from "@/components";
 import CustomDateSelector from "@/components/CustomDateSelector/CustomDateSelector";
 import { Link, Stack, Typography } from "@mui/material";
 import { useDispatch } from "react-redux";
@@ -43,9 +42,9 @@ function RegistrationForm() {
     year: "",
   };
 
-  const onSubmit = async (values, actions) => {
+  const onSubmit = (values, actions) => {
     const { email, username, name, password } = values;
-    const registrationResult = await dispatch(
+    dispatch(
       register({
         email,
         username,
@@ -53,10 +52,7 @@ function RegistrationForm() {
         password,
       })
     );
-
-    if (registrationResult.payload.success) {
-      navigate("channels/@me");
-    }
+    navigate("channel/@me", { replace: true });
     actions.resetForm();
   };
 
@@ -64,8 +60,6 @@ function RegistrationForm() {
     <Formik
       initialValues={initialValues}
       validationSchema={validationSchema}
-      onSubmit={onSubmit}
-    >
       onSubmit={onSubmit}
     >
       {({ isValid, setFieldValue }) => (
@@ -81,20 +75,7 @@ function RegistrationForm() {
                 fontSize: "1.5rem",
               }}
             >
-          <Stack sx={StyledStackSX}>
-            <Typography
-              variant="h2"
-              mb="12px"
-              sx={{
-                textAlign: "center",
-                fontWeight: "medium",
-                color: "#fff",
-                fontSize: "1.5rem",
-              }}
-            >
               Create an account
-            </Typography>
-            <Input
             </Typography>
             <Input
               id="email"
@@ -106,9 +87,6 @@ function RegistrationForm() {
             <Input id="name" label="Display name" name="name" />
             <Input id="username" label="Username" name="username" required />
             <Input
-            <Input id="name" label="Display name" name="name" />
-            <Input id="username" label="Username" name="username" required />
-            <Input
               id="password"
               label="Password"
               type="password"
@@ -117,36 +95,24 @@ function RegistrationForm() {
             />
             <CustomDateSelector setFieldValue={setFieldValue} sx={{ mb: 1 }} />
             <Button disabled={!isValid} type="submit" sx={{ mb: 3 }}>
-            <CustomDateSelector setFieldValue={setFieldValue} sx={{ mb: 1 }} />
-            <Button disabled={!isValid} type="submit" sx={{ mb: 3 }}>
               Continue
-            </Button>
-            <Typography sx={{ mb: 4, fontSize: "12px", color: "#ffffffbb" }}>
             </Button>
             <Typography sx={{ mb: 4, fontSize: "12px", color: "#ffffffbb" }}>
               By registering, you agree to Discord&apos;s&#32;
               <Link href="#" underline="none" sx={{ color: "#00a8fc" }}>
-              <Link href="#" underline="none" sx={{ color: "#00a8fc" }}>
                 Term&apos;s of Service&#32;
-              </Link>
               </Link>
               and &#32;
               <Link href="#" underline="none" sx={{ color: "#00a8fc" }}>
-              <Link href="#" underline="none" sx={{ color: "#00a8fc" }}>
                 Privacy Policy.
-              </Link>
-            </Typography>
               </Link>
             </Typography>
             <NavLink to="/login" className="text-sm font-medium text-[#00a8fc]">
               Already have an account?
             </NavLink>
           </Stack>
-          </Stack>
         </Form>
       )}
     </Formik>
   );
 }
-
-export default RegistrationForm;
