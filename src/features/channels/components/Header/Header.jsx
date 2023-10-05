f165737c9839738c429cdfab108be2fe6c3b6b8e
Bohdan <135275568+brightly-shining@users.noreply.github.com>
2023-10-05 15:06:18 +0200

    ✨ Make header buttons interactable (#27)

--- src/features/channels/components/Header/Header.jsx
+++ src/features/channels/components/Header/Header.jsx
@@ -1,7 +1,12 @@
-import { BiHash, BiSolidHelpCircle } from "react-icons/bi";
-import { useSelector } from "react-redux";
+import { BiHash, BiSolidHelpCircle, BiSolidBell } from "react-icons/bi";
+import { BsPinAngleFill } from "react-icons/bs";
+import { FaUserGroup } from "react-icons/fa6";
+import { useDispatch, useSelector } from "react-redux";
+import { IconButton } from "@/components";
+import { toggleUserList } from "@/redux/slices/uiSlice";
 
 export function Header() {
+  const dispatch = useDispatch();
   const title = useSelector((state) => state.server.currentServer.title);
   const channelName = useSelector((state) => state.server.currentChannel.name);
 
@@ -12,17 +17,17 @@ export function Header() {
       </div>
       <div className="flex grow justify-between bg-[#313338] px-4 py-3">
         <div className="flex items-center gap-x-1">
-          <BiHash
-            color="#80848e"
-            size={24}
-          />
+          <BiHash color="#80848e" size={24} />
           <h2 className="font-medium">{channelName}</h2>
         </div>
-        <div>
-          <BiSolidHelpCircle
-            color="#b5bac1"
-            size={24}
+        <div className="flex items-center gap-x-4 text-[#b5bac1]">
+          <IconButton icon={<BiSolidBell size={22} />} />
+          <IconButton icon={<BsPinAngleFill size={22} />} />
+          <IconButton
+            onClick={() => dispatch(toggleUserList())}
+            icon={<FaUserGroup size={22} />}
           />
+          <IconButton icon={<BiSolidHelpCircle size={24} />} />
         </div>
       </div>
     </header>
