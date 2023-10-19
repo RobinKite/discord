fd57fded91d74587435c32a2693bddb7a6b38c9f
Tadimm <139799853+Tadimm@users.noreply.github.com>
2023-10-19 21:05:30 +0300

    Settings v2 (#32)
    * wip
    
    * WIP
    
    * add todo
    
    * WIP with Settings styles
    
    * WIP with styles for Settings
    
    * finished with styles for Settings
    
    * work with devices in settings
    
    * bugfix
    
    ---------
    
    Co-authored-by: Alex Bird <horoshiy.ab@gmail.com>
    Co-authored-by: Alex Kite <33868185+RobinKite@users.noreply.github.com>

---
+++ src/components/Settings/TabContents/VoiceVideo.jsx
@@ -0,0 +1,120 @@
+import { MenuItem, Stack, Typography } from "@mui/material";
+import SettingsLine from "../SettingsLine";
+import {
+  Title,
+  CustomSelect,
+  PrettoSlider,
+  SettingsTitle,
+  MainButton,
+} from "../StyledElements";
+import MicTest from "../MicTest/MicTest";
+import camera from "../../../assets/camera.svg";
+import { useEffect, useState } from "react";
+
+export const VoiceVideoTabContent = () => {
+  const [inputDevice, setInputDevice] = useState("");
+  const [inputDevices, setInputDevices] = useState([]);
+  const [outputDevice, setOutputDevice] = useState("");
+  const [outputDevices, setOutputDevices] = useState([]);
+
+  const updateAudioDevices = async () => {
+    let localInputDevices = [];
+    let localOutputDevices = [];
+    try {
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      localInputDevices = devices.filter(
+        (device) => device.kind === "audioinput"
+      );
+      localOutputDevices = devices.filter(
+        (device) => device.kind === "audiooutput"
+      );
+      setInputDevices(localInputDevices);
+      setOutputDevices(localOutputDevices);
+      setInputDevice(inputDevices[0]?.deviceId || "");
+      setOutputDevice(outputDevices[0]?.deviceId || "");
+    } catch (err) {
+      console.error("Error receiving audio devices: ", err);
+    }
+  };
+
+  useEffect(() => {
+    updateAudioDevices();
+  }, []);
+
+  return (
+    <Stack spacing={3}>
+      <SettingsTitle>Voice Settings</SettingsTitle>
+      <Stack direction="row" spacing={3}>
+        <Stack sx={{ width: "320px" }}>
+          <Title>Input device</Title>
+          <CustomSelect
+            value={inputDevice}
+            onChange={(e) => setInputDevice(e.target.value)}
+          >
+            {inputDevices.map((device) => (
+              <MenuItem key={device.deviceId} value={device.deviceId}>
+                {device.label}
+              </MenuItem>
+            ))}
+          </CustomSelect>
+          <Title>Input Volume</Title>
+          <PrettoSlider
+            valueLabelDisplay="auto"
+            aria-label="pretto slider"
+            defaultValue={50}
+          />
+        </Stack>
+        <Stack sx={{ width: "320px" }}>
+          <Title>Output device</Title>
+          <CustomSelect
+            value={outputDevice}
+            onChange={(e) => setOutputDevice(e.target.value)}
+          >
+            {outputDevices.map((device) => (
+              <MenuItem key={device.deviceId} value={device.deviceId}>
+                {device.label}
+              </MenuItem>
+            ))}
+          </CustomSelect>
+          <Title>Output Volume</Title>
+          <PrettoSlider
+            valueLabelDisplay="auto"
+            aria-label="pretto slider"
+            defaultValue={50}
+          />
+        </Stack>
+      </Stack>
+      <Title>Mic Test</Title>
+      <Typography sx={{ color: "#b5bac1", fontSize: "14px", mb: "4px" }}>
+        Having mic issues? Start a test and say something fun—we&apos;ll play
+        your voice back to you.
+      </Typography>
+      <Stack direction="row" spacing={1}>
+        <MicTest />
+      </Stack>
+      <SettingsLine />
+      <SettingsTitle>Video Settings</SettingsTitle>
+      <Stack
+        alignItems="center"
+        justifyContent="center"
+        spacing={3}
+        sx={{
+          width: "660px",
+          height: "220px",
+          bgcolor: "#2b2d31",
+          border: "1px solid #1e1f22",
+          borderRadius: "4px",
+        }}
+      >
+        <Stack sx={{ width: "166px", height: "101px" }}>
+          <img src={camera} />
+        </Stack>
+        <MainButton>Test Video</MainButton>
+      </Stack>
+      <Title>Camera</Title>
+      <CustomSelect>
+        <MenuItem value="">Default</MenuItem>
+      </CustomSelect>
+    </Stack>
+  );
+};
